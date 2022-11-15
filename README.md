f80beb52d80ca0651c63e7f9b9ee5953803d5d3f
Dai An <anmusic2k7@gmail.com>
2022-11-15 21:02:49 +0700

    push trollbanbe2

---
+++ README.md
@@ -0,0 +1,2 @@
+### Đăng kí kênh của mình nha(https://www.youtube.com/watch?v=bKKCJ43cYsQ&t=54s)
+
---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Tớ có điều này muốn hỏi Cậu, Cậu nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Cậu thấy Tớ Đẹp zai phải không phải không ? 😙',
+    desc: 'Cậu không phải trả lời ấy :> ',
+    btnYes: 'Có, Cậu thật sự đẹp zai💖💖💖',
+    btnNo: '💔 Mày chơi đớ à! 💔 ',
+    question:'Nói cho Tớ biết đi vì sao vậy😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì Cậu đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Tớ biết mà😘😘',
+    messDesc: '💬💬 Nhắn tin cho Tớ nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/DaiAn.X.ThuTrang'
+}
